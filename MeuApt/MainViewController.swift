fe9d28c5013b0ba5771d27346c634a20cedc7c3d
Eduardo Domene Junior <eduardo.djr@hotmail.com>
2017-11-14 00:52:11 -0200

    Created custom table view cell.

--- MeuApt/MainViewController.swift
+++ MeuApt/MainViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var shots = [ShotModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Register cell
+        let nib = UINib(nibName: "CustomTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "CustomTableViewCellIdentifier")
+        
         // Do any additional setup after loading the view, typically from a nib.
         Dribble.sharedInstance.getShots(page: 0) { (error, response) in
             if let err = error{
                 print("\(err.localizedDescription)")
             }else if let resp = response{
-                print("\(resp)")
+                self.shots = resp
+                self.tableView.reloadData()
             }
         }
     }
@@ -26,7 +33,23 @@ class MainViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+//MARK: UITableViewDataSource
+extension MainViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.shots.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCellIdentifier", for: indexPath) as! CustomTableViewCell
+        cell.title.text = shots[indexPath.row].title
+        return cell
+    }
+}
+
+//MARK: UITableViewDelegate
+extension MainViewController: UITableViewDelegate{
     
 }
 
